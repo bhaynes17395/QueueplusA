42a863e0fcc9f691a073a7a414e9afc239ed27aa
bhaynes17395 <bhaynes17395@knights.ucf.edu>
2018-09-11 22:36:37 -0700

    Add files via upload

---
+++ scripts/login.js
@@ -0,0 +1,46 @@
+//var baseURL = "http://localhost:3306/API";
+var baseURL = "http://group5.gearhostpreview.com/API";
+// Login error responses
+var badLogin = "Incorrect username or password";
+
+function login(){
+
+    var email = document.getElementById("login-email").value;
+    var password = document.getElementById("login-password").value;
+
+    var payload = '{"email" : "' + email + '", "password" : "' + password + '"}';
+
+    var xhr = new XMLHttpRequest();
+    xhr.open("POST", baseURL + "/ProfLogin.php", false);
+    xhr.setRequestHeader("Content-type", "application/json; charset = UTF-8");
+
+    try {
+        xhr.onreadystatechange = function(){
+            if(xhr.readyState === 4){
+                var data = JSON.parse(xhr.responseText);
+                var error = data.error;
+
+                if(error != '') {
+                    printError(error);
+                    return;
+                }
+
+                document.getElementsByName("email")[0].value = "";
+                document.getElementsByName("password")[0].value = "";
+                window.location.href = "main.html";
+            }
+        }
+
+        xhr.send(payload);
+    }
+    catch(error) {
+        printError(error.message);
+    }
+    return false;
+}
+
+
+
+function printError(error){
+
+}
---
+++ scripts/register.js
@@ -0,0 +1,38 @@
+//var baseURL = "http://localhost:3306/API"
+var baseURL = "http://group5.gearhostpreview.com/API";
+var userID = 0;
+
+function addProfessor() {
+	var name = document.getElementById("form-name").value;
+	var email = document.getElementById("form-email").value;
+
+	var password = document.getElementById("form-password").value;
+	var passwordConfirm = document.getElementById("form-password-confirm").value;
+	if (password !== passwordConfirm) {
+		document.getElementsByName("response")[0].innerHTML = "Your passwords must match";
+		return;
+	}
+
+	// replace with appropriate varaible names
+	var payload = '{"name" : "' + name + '", "email" : "' + email + '", "password" : "' + password + '"}';
+
+	var xhr = new XMLHttpRequest();
+	xhr.open("POST", baseURL + "/AddProf.php", false);
+	xhr.setRequestHeader("Content-type", "application/json; charset = UTF-8");
+
+	try {
+		xhr.onreadystatechange = function(){
+			if(xhr.readyState === 4){
+				var data = JSON.parse(xhr.responseText);
+				var error = data.error;
+
+				window.location.href = "Login.html";
+			}
+		}
+
+		xhr.send(payload);
+	}
+	catch(error) {
+	}
+	return false;
+}
